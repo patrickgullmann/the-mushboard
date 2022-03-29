809a28e454c26c7e0ca8f0981947b1c64fa1d4f9
Patrick Gullmann <gullmannpatrick@gmail.com>
2022-03-29 18:33:45 +0200

    Imageboard Part 1 - Real Setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
+
+Vue.createApp({
+    data() {
+        return {
+            images: [],
+        };
+    },
+
+    mounted() {
+        // console.log("this", this);
+        fetch("/images")
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                this.images = data;
+            });
+    },
+
+    methods: {
+        sayHello: function () {
+            console.log(` Hello <3`);
+        },
+    },
+}).mount("#main");
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const db = require("./db");
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.use(express.json());
 
-app.get('*', (req, res) => {
+app.get("/images", (req, res) => {
+    db.getImages()
+        .then((result) => {
+            //console.log(result.rows);
+            res.json(result.rows);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(8080, () => console.log(`I'm listening.`));
+app.listen(8080, () => console.log(`🦑 I'm listening on 8080 🔥 🍑`));
